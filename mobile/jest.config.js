36e4e9bb87ad6730f3a4a69996aaa3e4f4044863
Ruhan Sahasi <ruhansahasi@icloud.com>
2026-03-16 20:53:36 -0400

    feat: add getOrCreateDeviceId utility for stable pre-auth device identity

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: "jest-expo",
   testMatch: ["**/__tests__/**/*.test.ts", "**/__tests__/**/*.test.tsx"],
   moduleNameMapper: { "^@/(.*)$": "<rootDir>/$1" },
+  transformIgnorePatterns: [
+    "node_modules/(?!((uuid|@react-native-async-storage|expo-secure-store)/)?)",
+  ],
 };
